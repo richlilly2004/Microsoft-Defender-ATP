5b5ae6efe32b4f29bb1f8bb7bbfb1bf30b02bd41
richlilly2004 <rich.lilly@outlook.com>
2020-05-06 07:33:29 -0500

    Create ConfigureAuditPol-FW.ps1

---
+++ PowerShell/ConfigureAuditPol-FW.ps1
@@ -0,0 +1,2 @@
+auditpol /set /subcategory:"Filtering Platform Packet Drop" /success:enable /failure:enable
+auditpol /set /subcategory:"Filtering Platform Connection" /success:enable /failure:enable
